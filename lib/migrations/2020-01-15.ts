cd539793c0082ce319523b8ddbcda990fecd62af
Radek Czemerys <7029942+radko93@users.noreply.github.com>
2020-09-10 15:26:53 +0200

    fix: use raw keychain value before migration (#104)

--- lib/migrations/2020-01-15.ts
+++ lib/migrations/2020-01-15.ts
@@ -309,7 +309,7 @@ export class Migration20200115 extends Migration {
       [ValueModesKeys.Wrapped]: {},
     };
 
-    const keychainValue = await this.services.deviceInterface.getNamespacedKeychainValue();
+    const keychainValue = await this.services.deviceInterface.getRawKeychainValue();
 
     const biometricPrefs = await this.services.deviceInterface.getJsonParsedStorageValue(
       LegacyKeys.MobileBiometricsPrefs
