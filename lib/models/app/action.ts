f35268f58c7a747e203596a718dfa4410f2b81a1
Baptiste Grob <60621355+baptiste-grob@users.noreply.github.com>
2020-09-24 16:34:30 +0200

    fix: give ids to actions, remove subrows, correctly initialize subactions (#116)
    * fix: give actions ID and correctly initialize subactions
    
    * chore: bundle

--- lib/models/app/action.ts
+++ lib/models/app/action.ts
@@ -13,11 +13,22 @@ export enum ActionVerb {
   Nested = 'nested'
 };
 
+let availableId = Number.MIN_SAFE_INTEGER;
+function getAvailableId() {
+  availableId += 1;
+  if (availableId === Number.MAX_SAFE_INTEGER) {
+    /** Wrap around */
+    availableId = Number.MIN_SAFE_INTEGER;
+  }
+  return availableId;
+}
+
 /**
  * An in-memory only construct for displaying a list of actions, as part of SNActionsExtension.
  */
 export class Action {
 
+  public readonly id = getAvailableId();
   public label!: string
   public desc!: string
   public running: boolean
@@ -28,14 +39,14 @@ export class Action {
   public url!: string
   public access_type!: ActionAccessType
   public readonly subactions?: Action[]
-  public subrows?: any[]
 
   constructor(json: any) {
     merge(this, json);
-    this.running = json.running || false;
-    this.error = json.error || false;
+    this.running = json.running ?? false;
+    this.error = json.error ?? false;
     if (this.lastExecuted) {
       this.lastExecuted = new Date(this.lastExecuted);
     }
+    this.subactions = json.subactions?.map((json: any) => new Action(json));
   }
 }
