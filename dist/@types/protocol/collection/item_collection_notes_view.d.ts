3058309cea9caff1b4f6525f91628294b1a45326
Baptiste Grob <60621355+baptiste-grob@users.noreply.github.com>
2020-10-12 11:46:55 +0200

    chore: bundle & tsc

--- dist/@types/protocol/collection/item_collection_notes_view.d.ts
+++ dist/@types/protocol/collection/item_collection_notes_view.d.ts
@@ -1,12 +1,12 @@
 import { SNNote, SNTag, SNSmartTag, SNItem } from '../../models';
-import { ItemCollection, CollectionSort, SortDirection } from "./item_collection";
+import { ItemCollection, CollectionSort, SortDirection } from './item_collection';
 /**
  * A view into ItemCollection that allows filtering by tag and smart tag.
  */
 export declare class ItemCollectionNotesView {
     private collection;
     private displayedList;
-    private tag?;
+    private tagUuid?;
     private needsRebuilding;
     constructor(collection: ItemCollection);
     notesMatchingSmartTag(smartTag: SNSmartTag, notes: SNNote[]): SNNote[];
