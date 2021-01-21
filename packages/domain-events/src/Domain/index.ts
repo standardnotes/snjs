9ed0fe9c760ea76e096a88a5e8360a34b1cf5ad4
Karol Sójko <karolsojko@gmail.com>
2021-01-21 14:42:00 +0100

    feat: add domain-events package

---
+++ packages/domain-events/src/Domain/index.ts
@@ -0,0 +1,12 @@
+export * from './Event/DomainEventFactoryInterface'
+export * from './Event/DomainEventInterface'
+export * from './Event/UserRegisteredEvent'
+export * from './Event/UserRegisteredEventPayload'
+
+export * from './Handler/DomainEventHandlerInterface'
+export * from './Handler/DomainEventMessageHandlerInterface'
+
+export * from './Publisher/DomainEventPublisherInterface'
+
+export * from './Subscriber/DomainEventSubscriberFactoryInterface'
+export * from './Subscriber/DomainEventSubscriberInterface'
---
+++ packages/domain-events/src/Infra/Redis/RedisDomainEventPublisher.ts
@@ -0,0 +1,19 @@
+import * as IORedis from 'ioredis'
+import * as zlib from 'zlib'
+
+import { DomainEventPublisherInterface } from '../../Domain/Publisher/DomainEventPublisherInterface'
+import { DomainEventInterface } from '../../Domain/Event/DomainEventInterface'
+
+export class RedisDomainEventPublisher implements DomainEventPublisherInterface{
+  constructor (
+    private redisClient: IORedis.Redis,
+    private eventChannel: string
+  ) {
+  }
+
+  async publish(event: DomainEventInterface): Promise<void> {
+    const message = zlib.deflateSync(JSON.stringify(event)).toString('base64')
+
+    await this.redisClient.publish(this.eventChannel, message)
+  }
+}
