5ac8135596f07599acbcc646ad45b2a5ad6bb0ea
Mo Bitar <me@bitar.io>
2021-03-06 12:53:37 -0600

    chore: single quotes for messages

--- packages/snjs/lib/services/api/messages.ts
+++ packages/snjs/lib/services/api/messages.ts
@@ -7,7 +7,8 @@ export const API_MESSAGE_GENERIC_INVALID_LOGIN =
   'A server error occurred while trying to sign in. Please try again.';
 export const API_MESSAGE_GENERIC_REGISTRATION_FAIL =
   'A server error occurred while trying to register. Please try again.';
-export const API_MESSAGE_GENERIC_CHANGE_PW_FAIL = `Something went wrong while changing your password. Your password was not changed. Please try again.`;
+export const API_MESSAGE_GENERIC_CHANGE_PW_FAIL =
+  'Something went wrong while changing your password. Your password was not changed. Please try again.';
 export const API_MESSAGE_GENERIC_SYNC_FAIL = 'Could not connect to server.';
 
 export const API_MESSAGE_REGISTRATION_IN_PROGRESS =
@@ -19,9 +20,11 @@ export const API_MESSAGE_CHANGE_PW_IN_PROGRESS =
 
 export const API_MESSAGE_FALLBACK_LOGIN_FAIL = 'Invalid email or password.';
 
-export const API_MESSAGE_GENERIC_TOKEN_REFRESH_FAIL = `A server error occurred while trying to refresh your session. Please try again.`;
+export const API_MESSAGE_GENERIC_TOKEN_REFRESH_FAIL =
+  'A server error occurred while trying to refresh your session. Please try again.';
 
-export const API_MESSAGE_TOKEN_REFRESH_IN_PROGRESS = `Your account session is being renewed with the server. Please try your request again.`;
+export const API_MESSAGE_TOKEN_REFRESH_IN_PROGRESS =
+  'Your account session is being renewed with the server. Please try your request again.';
 
 export const API_MESSAGE_RATE_LIMITED =
   'Too many successive server requests. Please wait a few minutes and try again.';
@@ -29,24 +32,30 @@ export const API_MESSAGE_RATE_LIMITED =
 export const API_MESSAGE_INVALID_SESSION =
   'Please sign in to an account in order to continue with your request.';
 
-export const UNSUPPORTED_PROTOCOL_VERSION = `This version of the application does not support your newer account type. Please upgrade to the latest version of Standard Notes to sign in.`;
+export const UNSUPPORTED_PROTOCOL_VERSION =
+  'This version of the application does not support your newer account type. Please upgrade to the latest version of Standard Notes to sign in.';
 
-export const EXPIRED_PROTOCOL_VERSION = `The protocol version associated with your account is outdated and no longer supported by this application. Please visit standardnotes.org/help/security for more information.`;
+export const EXPIRED_PROTOCOL_VERSION =
+  'The protocol version associated with your account is outdated and no longer supported by this application. Please visit standardnotes.org/help/security for more information.';
 
-export const OUTDATED_PROTOCOL_VERSION = `The encryption version for your account is outdated and requires upgrade. You may proceed with login, but are advised to perform a security update using the web or desktop application. Please visit standardnotes.org/help/security for more information.`;
+export const OUTDATED_PROTOCOL_VERSION =
+  'The encryption version for your account is outdated and requires upgrade. You may proceed with login, but are advised to perform a security update using the web or desktop application. Please visit standardnotes.org/help/security for more information.';
 
-export const UNSUPPORTED_KEY_DERIVATION = `Your account was created on a platform with higher security capabilities than this browser supports. If we attempted to generate your login keys here, it would take hours. Please use a browser with more up to date security capabilities, like Google Chrome or Firefox, to log in.`;
+export const UNSUPPORTED_KEY_DERIVATION =
+  'Your account was created on a platform with higher security capabilities than this browser supports. If we attempted to generate your login keys here, it would take hours. Please use a browser with more up to date security capabilities, like Google Chrome or Firefox, to log in.';
 
-export const INVALID_PASSWORD_COST = `Unable to sign in due to insecure password parameters. Please visit standardnotes.org/help/security for more information.`;
-export const INVALID_PASSWORD = `Invalid password.`;
+export const INVALID_PASSWORD_COST =
+  'Unable to sign in due to insecure password parameters. Please visit standardnotes.org/help/security for more information.';
+export const INVALID_PASSWORD = 'Invalid password.';
 
 export const OUTDATED_PROTOCOL_ALERT_TITLE = 'Update Recommended';
 export const OUTDATED_PROTOCOL_ALERT_IGNORE = 'Sign In';
-export const UPGRADING_ENCRYPTION = `Upgrading your account's encryption version…`;
+export const UPGRADING_ENCRYPTION =
+  "Upgrading your account's encryption version…";
 
-export const SETTING_PASSCODE = `Setting passcode…`;
-export const CHANGING_PASSCODE = `Changing passcode…`;
-export const REMOVING_PASSCODE = `Removing passcode…`;
+export const SETTING_PASSCODE = 'Setting passcode…';
+export const CHANGING_PASSCODE = 'Changing passcode…';
+export const REMOVING_PASSCODE = 'Removing passcode…';
 
 export const DO_NOT_CLOSE_APPLICATION =
   'Do not close the application until this process completes.';
@@ -64,8 +73,10 @@ export function StrictSignInFailed(
   return `Strict Sign In has refused the server's sign-in parameters. The latest account version is ${latest}, but the server is reporting a version of ${current} for your account. If you'd like to proceed with sign in anyway, please disable Strict Sign In and try again.`;
 }
 
-export const UNSUPPORTED_BACKUP_FILE_VERSION = `This backup file was created using a newer version of the application and cannot be imported here. Please update your application and try again.`;
-export const BACKUP_FILE_MORE_RECENT_THAN_ACCOUNT = `This backup file was created using a newer encryption version than your account's. Please run the available encryption upgrade and try again.`;
+export const UNSUPPORTED_BACKUP_FILE_VERSION =
+  'This backup file was created using a newer version of the application and cannot be imported here. Please update your application and try again.';
+export const BACKUP_FILE_MORE_RECENT_THAN_ACCOUNT =
+  "This backup file was created using a newer encryption version than your account's. Please run the available encryption upgrade and try again.";
 
 export const ImportStrings = {
   FileAccountPassword: 'File account password',
@@ -147,7 +158,7 @@ export const SessionStrings = {
   RecoverSession(email?: string): string {
     return email
       ? `Your credentials are needed for ${email} to refresh your session with the server.`
-      : `Your credentials are needed to refresh your session with the server.`;
+      : 'Your credentials are needed to refresh your session with the server.';
   },
   SessionRestored: 'Your session has been successfully restored.',
   EnterMfa: 'Please enter your two-factor authentication code.',
@@ -211,10 +222,12 @@ export const ErrorAlertStrings = {
     'We were unable to load your server session. This represents an inconsistency with your application state. Please take an opportunity to backup your data, then sign out and sign back in to resolve this issue.',
 
   StorageDecryptErrorTitle: 'Storage Error',
-  StorageDecryptErrorBody: `We were unable to decrypt your local storage. Please restart the app and try again. If you're unable to resolve this issue, and you have an account, you may try uninstalling the app then reinstalling, then signing back into your account. Otherwise, please contact help@standardnotes.org for support.`,
+  StorageDecryptErrorBody:
+    "We were unable to decrypt your local storage. Please restart the app and try again. If you're unable to resolve this issue, and you have an account, you may try uninstalling the app then reinstalling, then signing back into your account. Otherwise, please contact help@standardnotes.org for support.",
 };
 
 export const KeychainRecoveryStrings = {
   Title: 'Restore Keychain',
-  Text: `We've detected that your keychain has been wiped. This can happen when restoring your device from a backup. Please enter your account password to restore your account keys.`,
+  Text:
+    "We've detected that your keychain has been wiped. This can happen when restoring your device from a backup. Please enter your account password to restore your account keys.",
 };
