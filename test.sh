52f3984779278cc4aaa110879d8d505327757427
Karol Sójko <karolsojko@gmail.com>
2021-03-11 18:27:36 +0100

    feat: add new infra setup to SNJS test suite (#238)
    * feat: add new infra setup to SNJS test suite
    
    * fix: github actions
    
    * fix: ports and env vars
    
    * tmp: remove sample files
    
    * fix: syncing-server environment
    
    * fix: legacy syncing server environment to test
    
    * fix: add missing nginx proxy
    
    * fix: syncing-server-ruby setup
    
    * fix: uncomment docker compose building
    
    * fix: configure fixed images for services
    
    * fix: typo

--- test.sh
+++ test.sh
@@ -1,5 +1,11 @@
 # !/bin/bash
 
+echo "# Copying the sample configuration files"
+cp docker/api-gateway.env.sample docker/api-gateway.env
+cp docker/auth.env.sample docker/auth.env
+cp docker/syncing-server-js.env.sample docker/syncing-server-js.env
+cp docker/syncing-server.env.sample docker/syncing-server.env
+
 echo "# Installing project dependecies (Host Machine)"
 yarn install --pure-lockfile
 
@@ -10,23 +16,7 @@ function cleanup {
   docker-compose rm -vf
 }
 
-[ -n "${SYNCING_SERVER_VERSION}" ] || SYNCING_SERVER_VERSION=$1 && shift 1
-
-if [ -z "$SYNCING_SERVER_VERSION" ];
-then
-  echo "Syncing Server version is missing."
-  exit 1
-fi
-
-if [ "$SYNCING_SERVER_VERSION" != "js" ] && [ "$SYNCING_SERVER_VERSION" != "ruby" ];
-then
-  echo "Please put ruby or js (lowercase) as an argument."
-  exit 1
-fi
-
 cleanup
-
-export NGINX_REROUTE_HOST="http://syncing-server-$SYNCING_SERVER_VERSION"
 
 echo "# Pulling latest versions"
 docker-compose pull
@@ -34,37 +24,21 @@ docker-compose pull
 echo "# Building Docker images"
 docker-compose build
 
-echo "# Starting all containers for $SYNCING_SERVER_VERSION Test Suite"
+echo "# Starting all containers for Test Suite"
 docker-compose up -d
 
 attempt=0
-while [ $attempt -le 59 ]; do
+while [ $attempt -le 239 ]; do
     attempt=$(( $attempt + 1 ))
-    echo "# Waiting for Syncing Server Ruby to be up (attempt: $attempt)..."
-    result=$(docker-compose logs syncing-server-ruby)
-    if grep -q 'Starting Server' <<< $result ; then
+    echo "# Waiting for all services to be up (attempt: $attempt)..."
+    result=$(docker-compose logs api-gateway)
+    if grep -q 'Server started on port' <<< $result ; then
         sleep 2 # for warmup
-        echo "# Syncing Server Ruby is up!"
+        echo "#All services are up!"
         break
     fi
     sleep 2
 done
-
-if [ $SYNCING_SERVER_VERSION == "js" ]
-then
-  attempt=0
-  while [ $attempt -le 59 ]; do
-      attempt=$(( $attempt + 1 ))
-      echo "# Waiting for Syncing Server JS to be up (attempt: $attempt)..."
-      result=$(docker-compose logs syncing-server-js)
-      if grep -q 'Server started' <<< $result ; then
-          sleep 2 # for warmup
-          echo "# Syncing Server JS is up!"
-          break
-      fi
-      sleep 2
-  done
-fi
 
 attempt=0
 while [ $attempt -le 119 ]; do
@@ -78,15 +52,8 @@ while [ $attempt -le 119 ]; do
     sleep 2
 done
 
-if [ "$SYNCING_SERVER_VERSION" == "js" ];
-then
-  SKIP_TAG="syncing-server-ruby-only"
-else
-  SKIP_TAG="syncing-server-js-only"
-fi
-
-echo "# Starting $SYNCING_SERVER_VERSION Test Suite"
-npx mocha-headless-chrome --timeout 1200000 -f http://localhost:9001/test/test.html?skip=$SKIP_TAG
+echo "# Starting Test Suite"
+npx mocha-headless-chrome --timeout 1200000 -f http://localhost:9001/test/test.html
 test_result=$?
 
 cleanup
