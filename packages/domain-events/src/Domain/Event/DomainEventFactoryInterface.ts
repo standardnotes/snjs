c888fcb8b26a38c36de84cd8b971a3c59759506d
Karol Sójko <karolsojko@gmail.com>
2021-04-01 11:03:26 +0200

    feat: add ItemsSyncedEvent to domain-events (#244)

--- packages/domain-events/src/Domain/Event/DomainEventFactoryInterface.ts
+++ packages/domain-events/src/Domain/Event/DomainEventFactoryInterface.ts
@@ -1,5 +1,7 @@
+import { ItemsSyncedEvent } from './ItemsSyncedEvent'
 import { UserRegisteredEvent } from './UserRegisteredEvent'
 
 export interface DomainEventFactoryInterface {
   createUserRegisteredEvent(userUuid: string, email: string): UserRegisteredEvent
+  createItemsSyncedEvent(userUuid: string, extensionsServerUrl: string, extensionUuid: string, itemUuids: Array<string>, forceMute: boolean): ItemsSyncedEvent
 }
