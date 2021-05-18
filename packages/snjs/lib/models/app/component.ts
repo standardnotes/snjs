d48c2e69b2ca902cfc7dd80186e0aedc67839c7f
Johnny A <5891646+johnny243@users.noreply.github.com>
2021-05-18 15:58:29 -0400

    feat: KeyUp component action (#295)
    * feat: KeyUp component action
    
    * fix: rename KeyPressed to KeyDown
    
    Co-authored-by: Johnny Almonte <johnny243@users.noreply.github.com>

--- packages/snjs/lib/models/app/component.ts
+++ packages/snjs/lib/models/app/component.ts
@@ -42,7 +42,8 @@ export enum ComponentAction {
   SaveSuccess = 'save-success',
   SaveError = 'save-error',
   ThemesActivated = 'themes-activated',
-  KeyPressed = 'key-pressed'
+  KeyDown = 'key-down',
+  KeyUp = 'key-up'
 }
 
 export type ComponentPermission = {
