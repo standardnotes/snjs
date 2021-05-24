0c99f2b9362774872b7a56f5985df185125b78f2
Antonella Sgarlatta <antonella@standardnotes.org>
2021-05-24 16:51:07 -0300

    chore: add comment doc to application method

--- packages/snjs/lib/application.ts
+++ packages/snjs/lib/application.ts
@@ -773,6 +773,12 @@ export class SNApplication {
     return this.itemManager.findTagByTitle(title);
   }
 
+  /**
+   * Finds tags with title or component starting with a search query and (optionally) not associated with a note
+   * @param searchQuery - The query string to match
+   * @param note - The note whose tags should be omitted from results
+   * @returns Array containing tags matching search query and not associated with note
+   */
   public searchTags(searchQuery: string, note?: SNNote): SNTag[] {
     return this.itemManager.searchTags(searchQuery, note);
   }
