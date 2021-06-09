9b8d9d478c689c0f866c869d3a01d908682729c0
Antonella Sgarlatta <antonella@standardnotes.org>
2021-06-09 17:10:20 -0300

    feat: make tag titles case insensitive

--- packages/snjs/lib/services/item_manager.ts
+++ packages/snjs/lib/services/item_manager.ts
@@ -714,8 +714,8 @@ export class ItemManager extends PureService {
   /**
    * Finds the first tag matching a given title
    */
-  public findTagByTitle(title: string) {
-    return searchArray(this.tags, { title: title });
+  public findTagByTitle(title: string): SNTag | undefined {
+    return this.tags.find((tag) => tag.title.toLowerCase() === title.toLowerCase());
   }
 
   /**
