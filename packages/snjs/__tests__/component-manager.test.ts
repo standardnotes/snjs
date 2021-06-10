3788861d55acf4b1a89df8b18020d1d8ffdfde3b
Johnny Almonte <johnny243@users.noreply.github.com>
2021-06-10 02:59:13 -0400

    test(wip): component manager

--- packages/snjs/__tests__/component-manager.test.ts
+++ packages/snjs/__tests__/component-manager.test.ts
@@ -1,7 +1,38 @@
-import { Environment } from '../lib';
+import {
+  SNApplication,
+  SNComponent,
+  Platform,
+  Environment,
+  DeinitSource
+} from '../lib';
+import { createApplication } from './lib/appFactory';
+import { createComponentItem, testExtensionEditorPackage } from './helpers';
 
 describe('Component Manager', () => {
-  test('compiler is working', () => {
-    expect(Environment).toBeDefined();
+  /** The Standard Notes application. */
+  let testSNApp: SNApplication;
+  /** The test component. */
+  let testComponent: SNComponent;
+
+  beforeEach(async () => {
+    testSNApp = await createApplication('test-application', Environment.Web, Platform.LinuxWeb);
+    testComponent = await createComponentItem(testSNApp, testExtensionEditorPackage);
+  });
+
+  afterEach(() => {
+    testComponent = undefined;
+
+    testSNApp.deinit(DeinitSource.SignOut);
+    testSNApp = undefined;
+  });
+
+  test('isDesktop()', () => {
+    const { isDesktop } = testSNApp.componentManager;
+    expect(isDesktop).toBe(false);
+  });
+
+  test('isMobile()', () => {
+    const { isMobile } = testSNApp.componentManager;
+    expect(isMobile).toBe(false);
   });
 });
