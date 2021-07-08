27222706e4c37fa7337ddf44c2a2b1be3c96fea2
Antonella Sgarlatta <antonella@standardnotes.org>
2021-07-08 16:48:50 -0300

    feat: change permission and role types

--- packages/auth/src/Domain/Permission/Permission.ts
+++ packages/auth/src/Domain/Permission/Permission.ts
@@ -1,5 +1,12 @@
-export enum Permission {
+import { Uuid } from "../Uuid/Uuid";
+
+export enum PermissionName {
   SyncItems = 'SYNC_ITEMS',
   ExtendedNoteHistory = 'EXTENDED_NOTE_HISTORY',
   UnlimitedNoteHistory = 'UNLIMITED_NOTE_HISTORY'
+}
+
+export type Permission = {
+  uuid: Uuid;
+  name: PermissionName;
 }
--- packages/auth/src/Domain/Role/Role.ts
+++ packages/auth/src/Domain/Role/Role.ts
@@ -1,6 +1,13 @@
-export enum Role {
+import { Uuid } from "../Uuid/Uuid";
+
+export enum RoleName {
   User = 'USER',
   CoreUser = 'CORE_USER',
   PlusUser = 'PLUS_USER',
   ProUser = 'PRO_USER'
+}
+
+export type Role = {
+  uuid: Uuid;
+  name: RoleName;
 }
