c6af6522a1eaba0615c797b1e1f04e515e41f62b
Antonella Sgarlatta <antonella@standardnotes.org>
2021-07-19 11:12:29 -0300

    refactor: separate types into different files and use camelCase for feature properties

--- packages/auth/src/Domain/Feature/Feature.ts
+++ packages/auth/src/Domain/Feature/Feature.ts
@@ -1,67 +1,27 @@
 import { ComponentArea, ContentType } from '@standardnotes/snjs';
-
-export enum FeatureName {
-  MidnightTheme = 'theme:midnight',
-  FuturaTheme = 'theme:futura',
-  SolarizedDarkTheme = 'theme:solarized-dark',
-  AutobiographyTheme = 'theme:autobiography',
-  FocusedTheme = 'theme:focused',
-  TitaniumTheme = 'theme:titanium',
-  BoldEditor = 'editor:bold',
-  PlusEditor = 'editor:plus',
-  MarkdownBasicEditor = 'editor:markdown-basic',
-  MarkdownProEditor = 'editor:markdown-pro',
-  MarkdownMinimistEditor = 'editor:markdown-minimist',
-  TaskEditor = 'editor:task-editor',
-  CodeEditor = 'editor:code-editor',
-  TokenVaultEditor = 'editor:token-vault',
-  SheetsEditor = 'editor:sheets',
-  TwoFactorAuth = 'server:two-factor-auth',
-  NoteHistoryUnlimited = 'server:note-history-unlimited',
-  NoteHistory365Days = 'server:note-history-365-days',
-  NoteHistory30Days = 'server:note-history-30-days',
-  DailyEmailBackup = 'server:daily-email-backup',
-  DailyDropboxBackup = 'server:daily-dropbox-backup',
-  DailyGDriveBackup = 'server:daily-gdrive-backup',
-  DailyOneDriveBackup = 'server:daily-onedrive-backup',
-  Files25GB = 'server:files-25-gb',
-  Files5GB = 'server:files-5-gb',
-  TagNesting = 'app:tag-nesting',
-  Files = 'app:files',
-  CloudLink = 'component:cloud-link',
-  TwoFactorAuthManager = 'component:2fa-manager',
-  ListedCustomDomain = 'listed:custom-domain',
-}
-
-enum DockIconType {
-  Circle = 'circle',
-  Svg = 'svg',
-}
-
-enum Flag {
-  New = 'New',
-  Deprecated = 'Deprecated',
-}
+import { DockIconType } from './DockIconType';
+import { FeatureName } from './FeatureName';
+import { Flag } from './Flag';
 
 export type Feature = {
   name: FeatureName;
   identifier: string;
-  content_type: ContentType;
+  contentType: ContentType;
   area: ComponentArea;
   layerable: boolean;
-  no_mobile: boolean;
+  noMobile: boolean;
   version: string;
   description: string;
   url: string;
-  download_url: string;
-  marketing_url: string;
-  thumbnail_url: string;
+  downloadUrl: string;
+  marketingUrl: string;
+  thumbnailUrl: string;
   flags: Flag[];
-  dock_icon: {
+  dockIcon: {
     type: DockIconType;
-    background_color: string;
-    foreground_color: string;
-    border_color: string;
+    backgroundColor: string;
+    foregroundColor: string;
+    borderColor: string;
   };
-  expires_at: number;
+  expiresAt: number;
 };
