b01635f175286e6eb44ccf2c8a8f49c17037111b
Johnny Almonte <johnny243@users.noreply.github.com>
2021-07-19 20:36:03 -0400

    test(wip): item tests

--- packages/snjs/test/lib/item.test.js
+++ packages/snjs/test/lib/item.test.js
@@ -1,44 +1,40 @@
-/* eslint-disable no-unused-expressions */
-/* eslint-disable no-undef */
-import * as Factory from './lib/factory.js';
-chai.use(chaiAsPromised);
-const expect = chai.expect;
+import { PurePayload } from '@Lib/index';
+import { ContentType, SNItem, SNTag } from '@Lib/models';
+import * as Factory from '../factory';
 
 describe('item', () => {
-  beforeEach(function () {
-    this.createBarePayload = () => {
-      return new PurePayload({
-        uuid: '123',
-        content_type: ContentType.Note,
+  const createNote = () => {
+    return new SNItem(createBarePayload());
+  };
+
+  const createTag = (notes = []) => {
+    const references = notes.map((note) => {
+      return {
+        uuid: note.uuid,
+        content_type: note.content_type,
+      };
+    });
+    return new SNTag(
+      new PurePayload({
+        uuid: Factory.generateUuidish(),
+        content_type: ContentType.Tag,
         content: {
-          title: 'hello',
+          title: 'thoughts',
+          references: references,
         },
-      });
-    };
+      })
+    );
+  };
 
-    this.createNote = () => {
-      return new SNItem(this.createBarePayload());
-    };
-
-    this.createTag = (notes = []) => {
-      const references = notes.map((note) => {
-        return {
-          uuid: note.uuid,
-          content_type: note.content_type,
-        };
-      });
-      return new SNTag(
-        new PurePayload({
-          uuid: Factory.generateUuidish(),
-          content_type: ContentType.Tag,
-          content: {
-            title: 'thoughts',
-            references: references,
-          },
-        })
-      );
-    };
-  });
+  const createBarePayload = () => {
+    return new PurePayload({
+      uuid: '123',
+      content_type: ContentType.Note,
+      content: {
+        title: 'hello',
+      },
+    });
+  };
 
   it('constructing without uuid should throw', function () {
     const payload = new PurePayload({});
@@ -47,45 +43,45 @@ describe('item', () => {
       const item = new SNItem(payload);
       item;
     };
-    expect(throwFn).to.throw();
+    expect(throwFn).toThrowError();
   });
 
   it('healthy constructor', function () {
-    const item = this.createNote();
+    const item = createNote();
 
-    expect(item).to.be.ok;
-    expect(item.payload).to.be.ok;
+    expect(item).toBeTruthy();
+    expect(item.payload).toBeTruthy();
   });
 
   it('user modified date should be ok', function () {
-    const item = this.createNote();
+    const item = createNote();
 
-    expect(item.userModifiedDate).to.be.ok;
+    expect(item.userModifiedDate).toBeTruthy();
   });
 
   it('has relationship with item true', function () {
-    const note = this.createNote();
-    const tag = this.createTag();
+    const note = createNote();
+    const tag = createTag();
 
-    expect(tag.hasRelationshipWithItem(note)).to.equal(false);
+    expect(tag.hasRelationshipWithItem(note)).toBe(false);
   });
 
   it('has relationship with item true', function () {
-    const note = this.createNote();
-    const tag = this.createTag([note]);
+    const note = createNote();
+    const tag = createTag([note]);
 
-    expect(tag.hasRelationshipWithItem(note)).to.equal(true);
+    expect(tag.hasRelationshipWithItem(note)).toBe(true);
   });
 
   it('getDomainData for random domain should return undefined', function () {
-    const note = this.createNote();
+    const note = createNote();
 
-    expect(note.getDomainData('random')).to.not.be.ok;
+    expect(note.getDomainData('random')).toBeFalsy();
   });
 
   it('getDomainData for app domain should return object', function () {
-    const note = this.createNote();
+    const note = createNote();
 
-    expect(note.getDomainData(SNItem.DefaultAppDomain())).to.be.ok;
+    expect(note.getDomainData(SNItem.DefaultAppDomain())).toBeTruthy();
   });
 });
