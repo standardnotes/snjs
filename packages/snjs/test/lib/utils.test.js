ea0965d21bd2f0fe470521223953260547600adc
Johnny Almonte <johnny243@users.noreply.github.com>
2021-07-19 20:39:32 -0400

    test(wip): utils tests

--- packages/snjs/test/lib/utils.test.js
+++ packages/snjs/test/lib/utils.test.js
@@ -1,23 +1,39 @@
-/* eslint-disable no-unused-expressions */
-/* eslint-disable no-undef */
-chai.use(chaiAsPromised);
-const expect = chai.expect;
+import {
+  findInArray,
+  extendArray,
+  topLevelCompare,
+  jsonParseEmbeddedKeys,
+  omitUndefinedCopy,
+  dateSorted,
+  subtractFromArray,
+  removeFromArray,
+  removeFromIndex,
+  arrayByDifference,
+  uniqCombineObjArrays,
+  uniqueArrayByKey,
+  filterFromArray,
+  deepMerge,
+  truncateHexString,
+  isSameDay,
+  naturalSort,
+  isNullOrUndefined
+} from "@Lib/utils";
 
 describe('utils', () => {
   it('findInArray', async () => {
-    expect(findInArray).to.be.ok;
+    expect(findInArray).toBeTruthy();
     const array = [{ id: 1 }, { id: 2 }, { id: 3 }, { id: 4 }];
-    expect(findInArray(array, 'id', 1)).to.be.ok;
-    expect(findInArray(array, 'id', 'foo')).to.not.be.ok;
+    expect(findInArray(array, 'id', 1)).toBeTruthy();
+    expect(findInArray(array, 'id', 'foo')).toBeFalsy();
   });
 
   it('isNullOrUndefined', () => {
-    expect(isNullOrUndefined(null)).to.equal(true);
-    expect(isNullOrUndefined(undefined)).to.equal(true);
-    expect(isNullOrUndefined(1)).to.equal(false);
-    expect(isNullOrUndefined('foo')).to.equal(false);
-    expect(isNullOrUndefined({})).to.equal(false);
-    expect(isNullOrUndefined([null])).to.equal(false);
+    expect(isNullOrUndefined(null)).toBe(true);
+    expect(isNullOrUndefined(undefined)).toBe(true);
+    expect(isNullOrUndefined(1)).toBe(false);
+    expect(isNullOrUndefined('foo')).toBe(false);
+    expect(isNullOrUndefined({})).toBe(false);
+    expect(isNullOrUndefined([null])).toBe(false);
   });
 
   it('extendArray', () => {
@@ -25,17 +41,17 @@ describe('utils', () => {
     const original = array.slice();
     const extended = [4, 5, 6];
     extendArray(array, extended);
-    expect(array).to.eql(original.concat(extended));
+    expect(array).toEqual(original.concat(extended));
   });
 
   it('top level compare', () => {
     const left = { a: 1, b: 2 };
     const right = { a: 1, b: 2 };
     const middle = { a: 2, b: 1 };
-    expect(topLevelCompare(left, right)).to.equal(true);
-    expect(topLevelCompare(right, left)).to.equal(true);
-    expect(topLevelCompare(left, middle)).to.equal(false);
-    expect(topLevelCompare(middle, right)).to.equal(false);
+    expect(topLevelCompare(left, right)).toBe(true);
+    expect(topLevelCompare(right, left)).toBe(true);
+    expect(topLevelCompare(left, middle)).toBe(false);
+    expect(topLevelCompare(middle, right)).toBe(false);
   });
 
   it('jsonParseEmbeddedKeys', () => {
@@ -44,8 +60,8 @@ describe('utils', () => {
       b: JSON.stringify({ foo: 'bar' }),
     };
     const parsed = jsonParseEmbeddedKeys(object);
-    expect(typeof parsed.a).to.equal('object');
-    expect(typeof parsed.b).to.equal('object');
+    expect(typeof parsed.a).toBe('object');
+    expect(typeof parsed.b).toBe('object');
   });
 
   it('omitUndefined', () => {
@@ -54,7 +70,7 @@ describe('utils', () => {
       bar: undefined,
     };
     const omitted = omitUndefinedCopy(object);
-    expect(Object.keys(omitted).includes('bar')).to.equal(false);
+    expect(Object.keys(omitted).includes('bar')).toBe(false);
   });
 
   it('dateSorted', () => {
@@ -66,28 +82,28 @@ describe('utils', () => {
 
     /** ascending */
     const ascending = dateSorted(objects, 'date', true);
-    expect(ascending[0].date.getTime()).to.equal(5);
-    expect(ascending[1].date.getTime()).to.equal(7);
-    expect(ascending[2].date.getTime()).to.equal(10);
+    expect(ascending[0].date.getTime()).toBe(5);
+    expect(ascending[1].date.getTime()).toBe(7);
+    expect(ascending[2].date.getTime()).toBe(10);
 
     /** descending */
     const descending = dateSorted(objects, 'date', false);
-    expect(descending[0].date.getTime()).to.equal(10);
-    expect(descending[1].date.getTime()).to.equal(7);
-    expect(descending[2].date.getTime()).to.equal(5);
+    expect(descending[0].date.getTime()).toBe(10);
+    expect(descending[1].date.getTime()).toBe(7);
+    expect(descending[2].date.getTime()).toBe(5);
   });
 
   describe('subtractFromArray', () => {
     it('Removes all items appearing in the array', () => {
       const array = [1, 2, 3, 4, 5];
       subtractFromArray(array, [1, 3, 5]);
-      expect(array).to.eql([2, 4]);
+      expect(array).toEqual([2, 4]);
     });
 
     it('Ignores items not appearing in the array', () => {
       const array = [1, 2, 3, 4, 5];
       subtractFromArray(array, [0, 1, 3, 5]);
-      expect(array).to.eql([2, 4]);
+      expect(array).toEqual([2, 4]);
     });
   });
 
@@ -95,15 +111,15 @@ describe('utils', () => {
     it('Removes the first item appearing in the array', () => {
       const array = [1, 1, 2, 3];
       removeFromArray(array, 1);
-      expect(array).to.eql([1, 2, 3]);
+      expect(array).toEqual([1, 2, 3]);
       removeFromArray(array, 2);
-      expect(array).to.eql([1, 3]);
+      expect(array).toEqual([1, 3]);
     });
 
     it('Ignores items not appearing in the array', () => {
       const array = [1, 2, 3];
       removeFromArray(array, 0);
-      expect(array).to.eql([1, 2, 3]);
+      expect(array).toEqual([1, 2, 3]);
       removeFromArray(array, {});
     });
   });
@@ -111,14 +127,14 @@ describe('utils', () => {
   it('removeFromIndex', () => {
     const array = [1, 2, 3];
     removeFromIndex(array, 1);
-    expect(array).to.eql([1, 3]);
+    expect(array).toEqual([1, 3]);
   });
 
   it('arrayByDifference', () => {
     const array = [1, 2, 3, 4];
     const array2 = [2, 3];
     const result = arrayByDifference(array, array2);
-    expect(result).to.eql([1, 4]);
+    expect(result).toEqual([1, 4]);
   });
 
   it('uniqCombineObjArrays', () => {
@@ -129,7 +145,7 @@ describe('utils', () => {
     ];
 
     const result = uniqCombineObjArrays(arrayA, arrayB, ['a', 'b']);
-    expect(result.length).to.equal(2);
+    expect(result.length).toBe(2);
   });
 
   it('uniqueArrayByKey', () => {
@@ -137,14 +153,14 @@ describe('utils', () => {
     const arrayB = [{ uuid: 1 }, { uuid: 2 }, { uuid: 1 }, { uuid: 2 }];
 
     const result = uniqueArrayByKey(arrayA.concat(arrayB), ['uuid']);
-    expect(result.length).to.equal(2);
+    expect(result.length).toBe(2);
   });
 
   it('filterFromArray function predicate', () => {
     const array = [{ uuid: 1 }, { uuid: 2 }, { uuid: 3 }];
 
     filterFromArray(array, (o) => o.uuid === 1);
-    expect(array.length).to.equal(2);
+    expect(array.length).toBe(2);
   });
 
   it('lodash merge should behave as expected', () => {
@@ -160,7 +176,7 @@ describe('utils', () => {
     };
     // merging a with b should replace total content
     deepMerge(a, b);
-    expect(a.content.references).to.eql([]);
+    expect(a.content.references).toEqual([]);
   });
 
   it('truncates hex string', () => {
@@ -169,7 +185,7 @@ describe('utils', () => {
     const desiredBits = 128;
     const expectedLength = 32;
     const result = truncateHexString(hex256, desiredBits);
-    expect(result.length).to.equal(expectedLength);
+    expect(result.length).toBe(expectedLength);
   });
 
   describe('isSameDay', () => {
@@ -178,7 +194,7 @@ describe('utils', () => {
       const dateB = new Date(2021, 1, 16, 17, 30, 0);
 
       const result = isSameDay(dateA, dateB);
-      expect(result).to.equal(true);
+      expect(result).toBe(true);
     });
 
     it('returns false if two dates are not on the same day', () => {
@@ -186,7 +202,7 @@ describe('utils', () => {
       const dateB = new Date(2021, 1, 17, 17, 30, 0);
 
       const result = isSameDay(dateA, dateB);
-      expect(result).to.equal(false);
+      expect(result).toBe(false);
     });
   });
 
@@ -213,21 +229,21 @@ describe('utils', () => {
     });
     it('sorts elements in natural order in ascending direction by default', () => {
       const result = naturalSort(items, 'someProperty');
-      expect(result).lengthOf(items.length);
-      expect(result[0]).to.equal(items[4]);
-      expect(result[1]).to.equal(items[2]);
-      expect(result[2]).to.equal(items[0]);
-      expect(result[3]).to.equal(items[3]);
-      expect(result[4]).to.equal(items[1]);
+      expect(result).toHaveLength(items.length);
+      expect(result[0]).toBe(items[4]);
+      expect(result[1]).toBe(items[2]);
+      expect(result[2]).toBe(items[0]);
+      expect(result[3]).toBe(items[3]);
+      expect(result[4]).toBe(items[1]);
     });
     it('sorts elements in natural order in descending direction', () => {
       const result = naturalSort(items, 'someProperty', 'desc');
-      expect(result).lengthOf(items.length);
-      expect(result[0]).to.equal(items[1]);
-      expect(result[1]).to.equal(items[3]);
-      expect(result[2]).to.equal(items[0]);
-      expect(result[3]).to.equal(items[2]);
-      expect(result[4]).to.equal(items[4]);
+      expect(result).toHaveLength(items.length);
+      expect(result[0]).toBe(items[1]);
+      expect(result[1]).toBe(items[3]);
+      expect(result[2]).toBe(items[0]);
+      expect(result[3]).toBe(items[2]);
+      expect(result[4]).toBe(items[4]);
     });
   })
 });
