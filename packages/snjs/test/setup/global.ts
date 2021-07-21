c9080bb90e3f89a02cd49d76e9f5682735283047
Johnny Almonte <johnny243@users.noreply.github.com>
2021-07-21 02:05:59 -0400

    fix: handle uncaught exceptions

--- packages/snjs/test/setup/global.ts
+++ packages/snjs/test/setup/global.ts
@@ -2,3 +2,16 @@ import { version } from '../../package.json';
 
 //@ts-ignore
 global['__VERSION__'] = global['SnjsVersion'] = version;
+
+/*
+ * Handling uncaught exceptions.
+ * See https://github.com/facebook/jest/issues/3251
+ */
+if (!process.env.LISTENING_TO_UNHANDLED_REJECTION) {
+  process.on('unhandledRejection', reason => {
+    throw reason;
+  });
+  // @ts-ignore
+  // Avoid memory leak by adding too many listeners.
+  process.env.LISTENING_TO_UNHANDLED_REJECTION = true;
+}
