3ec29d72467799ea71400c951a6ec91ffae82189
Antonella Sgarlatta <antonella@standardnotes.org>
2021-07-29 11:24:41 -0300

    feat: remove fromRole from user role changed event payload

--- packages/domain-events/src/Domain/Event/UserRoleChangedEventPayload.ts
+++ packages/domain-events/src/Domain/Event/UserRoleChangedEventPayload.ts
@@ -3,7 +3,6 @@ import { RoleName } from '@standardnotes/auth'
 export interface UserRoleChangedEventPayload {
   userUuid: string
   email: string
-  fromRole: RoleName
   toRole: RoleName
   timestamp: number
 }
