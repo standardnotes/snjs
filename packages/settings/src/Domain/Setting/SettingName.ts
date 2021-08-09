af094d5bec50fb1278adfb9e84627c93905dcec7
Gorjan <mogi57@gmail.com>
2021-08-09 12:39:38 +0200

    fix: remove comments

--- packages/settings/src/Domain/Setting/SettingName.ts
+++ packages/settings/src/Domain/Setting/SettingName.ts
@@ -1,9 +1,3 @@
-/**
- * Enumerate all Settings with their names as identifiers
- *
- * Extend by adding more enum members, add payload shape to
- * {@link SettingPayload}
- */
 export enum SettingName {
   MfaSecret = 'MFA_SECRET',
 }
