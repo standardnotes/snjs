d8a047457ce90c770794d6cfb1bccb0793441ba3
Karol Sójko <karolsojko@gmail.com>
2021-08-10 11:44:21 +0200

    feat: add extension key to subscription renewed event

--- packages/domain-events/src/Domain/Event/SubscriptionRenewedEventPayload.ts
+++ packages/domain-events/src/Domain/Event/SubscriptionRenewedEventPayload.ts
@@ -2,6 +2,7 @@ import { SubscriptionName } from '@standardnotes/auth'
 
 export interface SubscriptionRenewedEventPayload {
   userEmail: string
+  extensionKey: string
   subscriptionName: SubscriptionName
   subscriptionExpiresAt: number
   timestamp: number
