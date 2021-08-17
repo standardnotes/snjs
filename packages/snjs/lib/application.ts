78723e1e6b81abbb43427989a1cae5386af5af84
Antonella Sgarlatta <antonella@standardnotes.org>
2021-08-17 17:53:44 -0300

    feat: migrate extension key

--- packages/snjs/lib/application.ts
+++ packages/snjs/lib/application.ts
@@ -1553,6 +1553,7 @@ export class SNApplication {
       challengeService: this.challengeService,
       itemManager: this.itemManager,
       singletonManager: this.singletonManager,
+      settingsService: this.settingsService,
       environment: this.environment,
       identifier: this.identifier,
     });
