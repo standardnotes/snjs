793841f3571f23c7e543ef1bdcfc4db29de55b45
Antonella Sgarlatta <antonella@standardnotes.org>
2021-09-13 19:33:13 -0300

    fix: fix e2e test

--- packages/snjs/mocha/migrations/migration.test.js
+++ packages/snjs/mocha/migrations/migration.test.js
@@ -109,7 +109,7 @@ describe('migrations', () => {
     await Factory.safeDeinit(application);
   });
 
-  it('should be 2 required migration coming from 1.0.0', async function () {
+  it('should be 4 required migration coming from 1.0.0', async function () {
     const application = await Factory.createAppWithRandNamespace();
     await application.deviceInterface.setRawStorageValue(
       'migrations',
@@ -122,7 +122,7 @@ describe('migrations', () => {
     const pendingMigrations = await SNMigrationService.getRequiredMigrations(
       await application.migrationService.getStoredSnjsVersion()
     );
-    expect(pendingMigrations.length).to.equal(3);
+    expect(pendingMigrations.length).to.equal(4);
     expect(pendingMigrations[0].version()).to.equal('2.0.0');
     await application.prepareForLaunch({
       receiveChallenge: () => {},
