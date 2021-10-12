524a46591b5ca58a2286e83186d551b44f0593b3
Karol Sójko <karolsojko@gmail.com>
2021-10-12 15:07:39 +0200

    feat: offline subscriptions update to events (#451)
    * feat: add offline option for subscription events
    
    * fix: add subscription offline information to the extension key granted email
    
    * fix: add optional subscription name to the extension key granted event

--- packages/domain-events/src/Domain/Event/ExtensionKeyGrantedEventPayload.ts
+++ packages/domain-events/src/Domain/Event/ExtensionKeyGrantedEventPayload.ts
@@ -1,5 +1,10 @@
+import { SubscriptionName } from '@standardnotes/auth'
+
 export interface ExtensionKeyGrantedEventPayload {
   userEmail: string
   extensionKey: string
   timestamp: number
+  offline: boolean
+  origin: 'create-user' | 'update-subscription'
+  subscriptionName: SubscriptionName | null
 }
