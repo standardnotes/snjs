2be5985637ac60dad9a7fb3e78c0130105946f9d
Karol Sójko <karolsojko@protonmail.com>
2021-10-12 14:06:53 +0200

    fix: add subscription offline information to the extension key granted email

--- packages/domain-events/src/Domain/Event/ExtensionKeyGrantedEventPayload.ts
+++ packages/domain-events/src/Domain/Event/ExtensionKeyGrantedEventPayload.ts
@@ -1,5 +1,10 @@
+import { SubscriptionName } from '@standardnotes/auth'
+
 export interface ExtensionKeyGrantedEventPayload {
   userEmail: string
   extensionKey: string
   timestamp: number
+  subscriptionName: SubscriptionName
+  offline: boolean
+  origin: 'create-user' | 'update-subscription'
 }
