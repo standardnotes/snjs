9abb265e895e16affe4a626d2af7692909e5559b
Karol Sójko <karolsojko@gmail.com>
2021-10-15 08:19:09 +0200

    fix: rename dashboard token event to offline subscription token (#455)

--- packages/domain-events/src/Domain/Event/DashboardTokenCreatedEvent.ts
+++
@@ -1,8 +0,0 @@
-import { DomainEventInterface } from './DomainEventInterface'
-
-import { DashboardTokenCreatedEventPayload } from './DashboardTokenCreatedEventPayload'
-
-export interface DashboardTokenCreatedEvent extends DomainEventInterface {
-  type: 'DASHBOARD_TOKEN_CREATED'
-  payload: DashboardTokenCreatedEventPayload
-}
--- packages/domain-events/src/Domain/Event/DashboardTokenCreatedEventPayload.ts
+++
@@ -1,4 +0,0 @@
-export interface DashboardTokenCreatedEventPayload {
-  token: string
-  email: string
-}
