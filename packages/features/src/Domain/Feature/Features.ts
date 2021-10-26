693048bccfce57df7b88f8f87c1e3ccbe34fd93a
Mo Bitar <me@bitar.io>
2021-10-26 12:29:00 -0500

    feat: add missing features

--- packages/features/src/Domain/Feature/Features.ts
+++ packages/features/src/Domain/Feature/Features.ts
@@ -20,19 +20,19 @@ const validateFeatureItem = (featureItem: TFeatureItemFromJson) => {
   const flagTypes = Object.values(ComponentFlag)
 
   if (!featureIdentifiers.includes(identifier as FeatureIdentifier)) {
-    throw Error('Invalid feature identifier')
+    throw Error(`Invalid feature identifier ${identifier}`)
   }
   if (!permissionNames.includes(permission_name as PermissionName)) {
-    throw Error('Invalid permission name')
+    throw Error(`Invalid permission name ${permission_name}`)
   }
   if (content_type && !contentTypes.includes(content_type as ContentType)) {
-    throw Error('Invalid feature content type')
+    throw Error(`Invalid feature content type ${content_type}`)
   }
   if (area && !componentArea.includes(area as ComponentArea)) {
-    throw Error('Invalid feature area')
+    throw Error(`Invalid feature area ${area}`)
   }
   if (flags && flags.some((flag) => !flagTypes.includes(flag as ComponentFlag))) {
-    throw Error('Invalid feature flag')
+    throw Error(`Invalid feature flag ${flags}`)
   }
 }
 
