504cf10d83c9cba6e8ee79ce138847a293a2f9e0
Karol Sójko <karolsojko@protonmail.com>
2021-10-27 10:06:01 +0200

    feat: add content decoder to common package

--- packages/common/src/Domain/Content/ContentType.ts
+++ packages/common/src/Domain/Content/ContentType.ts
@@ -1,3 +1,4 @@
+/* istanbul ignore file */
 export enum ContentType {
   Any = '*',
   Item = 'SF|Item',
--- packages/common/src/Domain/index.ts
+++ packages/common/src/Domain/index.ts
@@ -1,3 +1,5 @@
 export * from './Content/ContentType'
+export * from './Content/ContentDecoder'
+export * from './Content/ContentDecoderInterface'
 export * from './DataType/Uuid'
 export * from './Error/ErrorTag'
