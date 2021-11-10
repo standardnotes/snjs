7dae6ef672953e8b5a9e58e3a32f3f7bc9f30d9c
Aman Harwara <amanharwara@protonmail.com>
2021-11-10 17:02:55 +0530

    feat: Add activeUntil date and rename pricePaid to payAmount

--- packages/domain-events/src/Domain/Event/ExtensionKeyGrantedEventPayload.ts
+++ packages/domain-events/src/Domain/Event/ExtensionKeyGrantedEventPayload.ts
@@ -1,13 +1,14 @@
-import { SubscriptionName } from '@standardnotes/auth'
+import { SubscriptionName } from '@standardnotes/auth';
 
 export interface ExtensionKeyGrantedEventPayload {
-  userEmail: string
-  extensionKey: string
-  timestamp: number
-  offline: boolean
-  origin: 'create-user' | 'update-subscription'
-  subscriptionName: SubscriptionName | null
-  offlineFeaturesToken: string
-  pricePaid: string
-  billingFrequency: number
+  userEmail: string;
+  extensionKey: string;
+  timestamp: number;
+  offline: boolean;
+  origin: 'create-user' | 'update-subscription';
+  subscriptionName: SubscriptionName | null;
+  offlineFeaturesToken: string;
+  payAmount: number;
+  billingFrequency: number;
+  activeUntil: string;
 }
