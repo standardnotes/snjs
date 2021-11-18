13e4794d562bfcba1ae877694bf395e03b780ea6
Karol Sójko <karolsojko@gmail.com>
2021-11-18 09:30:33 +0100

    feat: add role name to features and refactor package structure

--- packages/features/src/Domain/Feature/FeatureDescription.ts
+++ packages/features/src/Domain/Feature/FeatureDescription.ts
@@ -1,25 +1,11 @@
 import { ContentType } from '@standardnotes/common'
+import { RoleName } from '@standardnotes/auth'
 import { ComponentArea } from '../Component/ComponentArea'
 import { PermissionName } from '../Permission/PermissionName'
 import { FeatureIdentifier } from './FeatureIdentifier'
-import { ComponentFlag } from './Flag'
-
-export type ThemeDockIcon = {
-  type: 'svg' | 'circle';
-  background_color: string;
-  foreground_color: string;
-  border_color: string;
-  source?: string;
-};
-
-export enum NoteType {
-  Authentication = 'authentication',
-  Code = 'code',
-  Markdown = 'markdown',
-  RichText = 'rich-text',
-  Spreadsheet = 'spreadsheet',
-  Task = 'task',
-}
+import { ComponentFlag } from '../Component/ComponentFlag'
+import { NoteType } from '../Component/NoteType'
+import { ThemeDockIcon } from '../Component/ThemeDockIcon'
 
 export type FeatureDescription = {
   area?: ComponentArea;
@@ -31,6 +17,7 @@ export type FeatureDescription = {
   dock_icon?: ThemeDockIcon;
   download_url: string;
   expires_at?: number;
+  role_name?: RoleName;
   file_type: 'txt' | 'html' | 'md' | 'json';
   flags?: ComponentFlag[];
   identifier: FeatureIdentifier;
