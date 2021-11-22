d86b423db76742f14bbb354cd8695969eea792b8
Karol Sójko <karolsojko@protonmail.com>
2021-11-22 20:25:50 +0100

    fix: add offline indicator to subscription reassigned event

--- packages/domain-events/src/Domain/Event/SubscriptionReassignedEventPayload.ts
+++ packages/domain-events/src/Domain/Event/SubscriptionReassignedEventPayload.ts
@@ -3,6 +3,7 @@ import { SubscriptionName } from '@standardnotes/auth'
 export interface SubscriptionReassignedEventPayload {
   userEmail: string
   extensionKey: string
+  offline: boolean
   subscriptionId: number
   subscriptionName: SubscriptionName
   subscriptionExpiresAt: number
