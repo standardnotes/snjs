461c806354bbe7413b14d10d89c44279801cc338
Karol Sójko <karolsojko@protonmail.com>
2021-12-03 11:33:39 +0100

    fix: remove temporary ITEMS_CONTENT_SIZE_RECALCULATION_REQUESTED event

--- packages/domain-events/src/Domain/Event/ItemsContenSizeRecalculationRequestedEvent.ts
+++
@@ -1,7 +0,0 @@
-import { DomainEventInterface } from './DomainEventInterface'
-import { ItemsContentSizeRecalculationRequestedEventPayload } from './ItemsContentSizeRecalculationRequestedEventPayload'
-
-export interface ItemsContentSizeRecalculationRequestedEvent extends DomainEventInterface {
-  type: 'ITEMS_CONTENT_SIZE_RECALCULATION_REQUESTED'
-  payload: ItemsContentSizeRecalculationRequestedEventPayload
-}
