0241dd984bf82b962da81f8bd557b4e5184e37da
Karol Sójko <karolsojko@protonmail.com>
2021-12-10 13:54:45 +0100

    feat: add new domain event interface correlation structure

--- packages/domain-events/src/Domain/Event/DomainEventInterface.ts
+++ packages/domain-events/src/Domain/Event/DomainEventInterface.ts
@@ -2,4 +2,10 @@ export interface DomainEventInterface {
   type: string
   createdAt: Date
   payload: unknown
+  meta: {
+    correlation: {
+      userIdentifier: string
+      userIdentifierType: 'uuid' | 'email'
+    }
+  }
 }
