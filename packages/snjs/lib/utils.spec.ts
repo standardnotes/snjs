da8bb7b0d4d629a4ae1f54bf4884ac81904c8363
Mo <me@bitar.io>
2021-12-22 12:22:48 -0600

    feat: add dompurify utility function (#512)
    * feat: add dompurify utility function
    
    * chore: lint
    
    * fix: auth and domain-events dependencies

---
+++ packages/snjs/lib/utils.spec.ts
@@ -0,0 +1,13 @@
+import createDOMPurify from 'dompurify';
+import { JSDOM } from 'jsdom';
+
+const window = new JSDOM('').window;
+const DOMPurify = createDOMPurify(window as never);
+
+describe.only('utils', () => {
+  it('sanitizeHtmlString', () => {
+    const dirty = '<svg><animate onbegin=alert(1) attributeName=x dur=1s>';
+    const cleaned = DOMPurify.sanitize(dirty);
+    expect(cleaned).toEqual('<svg></svg>');
+  });
+});
