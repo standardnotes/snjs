c27827f8c7969dd32511c9c75122ece372132c83
Karol Sójko <karolsojko@gmail.com>
2021-12-23 11:58:59 +0100

    feat: add one drive backup frequency setting (#522)

--- packages/settings/src/Domain/Setting/SettingName.ts
+++ packages/settings/src/Domain/Setting/SettingName.ts
@@ -3,6 +3,7 @@ export enum SettingName {
   ExtensionKey = 'EXTENSION_KEY',
   EmailBackup = 'EMAIL_BACKUP',
   DropboxBackupFrequency = 'DROPBOX_BACKUP_FREQUENCY',
+  OneDriveBackupFrequency = 'ONE_DRIVE_BACKUP_FREQUENCY',
   MuteFailedBackupsEmails = 'MUTE_FAILED_BACKUPS_EMAILS',
   MuteFailedCloudBackupsEmails = 'MUTE_FAILED_CLOUD_BACKUPS_EMAILS',
 }
