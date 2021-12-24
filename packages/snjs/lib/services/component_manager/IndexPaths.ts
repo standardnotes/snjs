372d83f70ec57df339b3d0727a533cc5628f3b7f
Mo <mo@standardnotes.com>
2021-12-24 11:15:23 -0600

    chore: remove unused file

--- packages/snjs/lib/services/component_manager/IndexPaths.ts
+++
@@ -1,5 +0,0 @@
-import { FeatureIdentifier } from '@standardnotes/features';
-
-export const FeatureIndexPaths: Partial<Record<FeatureIdentifier, string>> = {
-  [FeatureIdentifier.CodeEditor]: 'index.html',
-};
