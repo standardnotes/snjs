7c590e19a20c7e5bcb6425643a579a66d8f1dd79
Karol Sójko <karolsojko@gmail.com>
2021-12-28 11:59:09 +0100

    feat: add subscription sync requested event (#530)

---
+++ packages/domain-events/src/Domain/Event/SubscriptionSyncRequestedEventPayload.ts
@@ -0,0 +1,15 @@
+import { SubscriptionName } from '@standardnotes/auth'
+
+export interface SubscriptionSyncRequestedEventPayload {
+  userEmail: string
+  subscriptionId: number
+  subscriptionName: SubscriptionName
+  subscriptionExpiresAt: number
+  timestamp: number
+  offline: boolean
+  extensionKey: string
+  offlineFeaturesToken: string
+  payAmount: number | null
+  billingEveryNMonths: number | null
+  activeUntil: string | null
+}
