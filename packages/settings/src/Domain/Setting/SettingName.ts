40ec0ee61ee51262125af513d7cedf0e9a3c399d
Karol Sójko <karolsojko@protonmail.com>
2021-12-29 12:21:35 +0100

    fix: add one drive backup token setting name

--- packages/settings/src/Domain/Setting/SettingName.ts
+++ packages/settings/src/Domain/Setting/SettingName.ts
@@ -7,6 +7,7 @@ export enum SettingName {
   DropboxBackupToken = 'DROPBOX_BACKUP_TOKEN',
   OneDriveBackupFrequency = 'ONE_DRIVE_BACKUP_FREQUENCY',
   OneDriveBackupUrl = 'ONE_DRIVE_BACKUP_URL',
+  OneDriveBackupToken = 'ONE_DRIVE_BACKUP_TOKEN',
   GoogleDriveBackupFrequency = 'GOOGLE_DRIVE_BACKUP_FREQUENCY',
   GoogleDriveBackupUrl = 'GOOGLE_DRIVE_BACKUP_URL',
   MuteFailedBackupsEmails = 'MUTE_FAILED_BACKUPS_EMAILS',
