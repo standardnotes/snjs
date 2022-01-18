e2b85752ada2d32d0c3deaedf7014d5293de4315
Karol Sójko <karolsojko@protonmail.com>
2022-01-18 09:30:39 +0100

    feat: add file upload settings

--- packages/settings/src/Domain/Setting/SettingName.ts
+++ packages/settings/src/Domain/Setting/SettingName.ts
@@ -10,4 +10,6 @@ export enum SettingName {
   GoogleDriveBackupToken = 'GOOGLE_DRIVE_BACKUP_TOKEN',
   MuteFailedBackupsEmails = 'MUTE_FAILED_BACKUPS_EMAILS',
   MuteFailedCloudBackupsEmails = 'MUTE_FAILED_CLOUD_BACKUPS_EMAILS',
+  FileUploadBytesLimit = 'FILE_UPLOAD_BYTES_LIMIT',
+  FileUploadBytesUsed = 'FILE_UPLOAD_BYTES_USED',
 }
