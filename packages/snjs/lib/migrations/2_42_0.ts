3bf49eec3f4d239b9cbdac7ad3fd98a1b8315280
Aman Harwara <amanharwara@protonmail.com>
2022-01-20 21:27:55 +0530

    chore: bump migration version

--- packages/snjs/lib/migrations/2_42_0.ts
+++ packages/snjs/lib/migrations/2_42_0.ts
@@ -1,11 +1,11 @@
 import { ApplicationStage } from '../stages';
 import { Migration } from '@Lib/migrations/migration';
-import { ContentType, SNItem, SNTheme } from '@Lib/models';
-import { FeatureIdentifier } from '..';
+import { SNItem, SNTheme } from '@Lib/models';
+import { ContentType, FeatureIdentifier } from '..';
 
-export class Migration2_32_0 extends Migration {
+export class Migration2_42_0 extends Migration {
   static version(): string {
-    return '2.32.0';
+    return '2.42.0';
   }
 
   protected registerStageHandlers(): void {
