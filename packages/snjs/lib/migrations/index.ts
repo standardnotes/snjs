392e46465d90acad87605ebe23a322c3ff0b723c
Laurent Senta <laurent@singulargarden.com>
2022-01-20 12:05:01 +0100

    fix: import migration 3.0.0

--- packages/snjs/lib/migrations/index.ts
+++ packages/snjs/lib/migrations/index.ts
@@ -4,3 +4,4 @@ export { Migration2_7_0 } from './2_7_0';
 export { Migration2_20_0 } from './2_20_0';
 export { Migration2_36_0 } from './2_36_0';
 export { Migration2_42_0 } from './2_42_0';
+export { Migration3_0_0 } from './3_0_0';
