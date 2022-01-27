ee006a5f399f560f44664bac97c24999d05a5e16
Aman Harwara <amanharwara@protonmail.com>
2022-01-27 23:10:59 +0530

    feat: add names to some unnamed features

--- packages/features/src/Domain/Feature/Features.ts
+++ packages/features/src/Domain/Feature/Features.ts
@@ -481,22 +481,27 @@ function nonEditorComponents(): IframeComponentFeatureDescription[] {
 function serverFeatures(): ServerFeatureDescription[] {
   return [
     {
+      name: 'Two factor authentication',
       identifier: FeatureIdentifier.TwoFactorAuth,
       permission_name: PermissionName.TwoFactorAuth,
     },
     {
+      name: 'Unlimited note history',
       identifier: FeatureIdentifier.NoteHistoryUnlimited,
       permission_name: PermissionName.NoteHistoryUnlimited,
     },
     {
+      name: '365 days note history',
       identifier: FeatureIdentifier.NoteHistory365Days,
       permission_name: PermissionName.NoteHistory365Days,
     },
     {
+      name: '30 days note history',
       identifier: FeatureIdentifier.NoteHistory30Days,
       permission_name: PermissionName.NoteHistory30Days,
     },
     {
+      name: 'Email backups',
       identifier: FeatureIdentifier.DailyEmailBackup,
       permission_name: PermissionName.DailyEmailBackup,
     },
@@ -539,7 +544,7 @@ function clientFeatures(): ClientFeatureDescription[] {
         'Create smart filters for viewing notes matching specific criteria.',
     },
     {
-      name: '',
+      name: 'Encrypted files',
       identifier: FeatureIdentifier.Files,
       permission_name: PermissionName.Files,
       description: '',
