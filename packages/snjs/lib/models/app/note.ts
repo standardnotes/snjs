6313e77a5a155000382c37771ac2de981e8f6f64
Mo <mo@standardnotes.com>
2022-02-07 08:53:53 -0600

    fix: embedded safe text and title for note

--- packages/snjs/lib/models/app/note.ts
+++ packages/snjs/lib/models/app/note.ts
@@ -20,22 +20,19 @@ export const isNote = (x: SNItem): x is SNNote =>
 
 /** A note item */
 export class SNNote extends SNItem implements NoteContent {
-  public readonly title!: string;
-  /* Some external editors can't handle a null value for text.
-   * Notes created on mobile with no text have a null value for it,
-   * so we'll just set a default here. */
-  public readonly text: string = '';
+  public readonly title: string;
+  public readonly text: string;
   public readonly mobilePrefersPlainEditor?: boolean;
   public readonly hidePreview = false;
-  public readonly preview_plain!: string;
-  public readonly preview_html!: string;
+  public readonly preview_plain: string;
+  public readonly preview_html: string;
   public readonly prefersPlainEditor!: boolean;
   public readonly spellcheck?: boolean | undefined;
 
   constructor(payload: PurePayload) {
     super(payload);
-    this.title = this.payload.safeContent.title;
-    this.text = this.payload.safeContent.text;
+    this.title = this.payload.safeContent.title || '';
+    this.text = this.payload.safeContent.text || '';
     this.preview_plain = this.payload.safeContent.preview_plain;
     this.preview_html = this.payload.safeContent.preview_html;
     this.hidePreview = this.payload.safeContent.hidePreview;
@@ -49,14 +46,6 @@ export class SNNote extends SNItem implements NoteContent {
     if (!isNullOrUndefined(this.payload.safeContent.mobilePrefersPlainEditor)) {
       this.mobilePrefersPlainEditor = this.payload.safeContent.mobilePrefersPlainEditor;
     }
-  }
-
-  safeText(): string {
-    return this.text || '';
-  }
-
-  safeTitle(): string {
-    return this.title || '';
   }
 }
 
