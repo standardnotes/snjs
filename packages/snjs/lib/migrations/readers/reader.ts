fca657341b7fc7e69a37f909002f65d575422173
Mo <mo@standardnotes.com>
2022-02-28 09:46:24 -0600

    chore: upgrade packages/snjs deps

--- packages/snjs/lib/migrations/readers/reader.ts
+++ packages/snjs/lib/migrations/readers/reader.ts
@@ -11,22 +11,22 @@ export abstract class StorageReader {
   constructor(
     protected deviceInterface: DeviceInterface,
     protected identifier: ApplicationIdentifier,
-    protected environment: Environment
+    protected environment: Environment,
   ) {}
 
   public static version(): string {
     throw Error('Must override');
   }
 
-  public abstract async getAccountKeyParams(): Promise<unknown | undefined>;
+  public abstract getAccountKeyParams(): Promise<unknown | undefined>;
 
   /**
    * Returns true if the state of storage has account keys present
    * in version-specific storage (either keychain or raw storage)
    */
-  public abstract async hasNonWrappedAccountKeys(): Promise<boolean>;
+  public abstract hasNonWrappedAccountKeys(): Promise<boolean>;
 
-  public abstract async hasPasscode(): Promise<boolean>;
+  public abstract hasPasscode(): Promise<boolean>;
 
   /** Whether this version used the keychain to store keys */
   public abstract usesKeychain(): boolean;
