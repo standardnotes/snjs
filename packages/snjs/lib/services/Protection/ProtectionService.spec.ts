4c1ff2838ab12f1615e0e28126707b6dd8977780
Mo <mo@standardnotes.com>
2022-03-06 10:55:19 -0600

    fix: unit tests

--- packages/snjs/lib/services/Protection/ProtectionService.spec.ts
+++ packages/snjs/lib/services/Protection/ProtectionService.spec.ts
@@ -3,17 +3,12 @@ import { ItemManager } from '../Items/ItemManager'
 import { SNProtocolService } from '../ProtocolService'
 import { SNStorageService } from '../StorageService'
 import { SNProtectionService } from './ProtectionService'
-import {
-  DeviceInterface,
-  InternalEventBus,
-  InternalEventBusInterface,
-} from '@standardnotes/services'
+import { InternalEventBus, InternalEventBusInterface } from '@standardnotes/services'
 import { UuidGenerator } from '@standardnotes/utils'
-import { PayloadManager } from '../PayloadManager'
-import { SNPureCrypto } from '@standardnotes/sncrypto-common'
 import { SNFile } from '@Lib/models'
 import { ContentType } from '@standardnotes/common'
 import { FillItemContent, CreateMaxPayloadFromAnyObject } from '@standardnotes/payloads'
+import { ChallengeReason } from '@Lib/challenges'
 
 const setupRandomUuid = () => {
   UuidGenerator.SetGenerator(() => String(Math.random()))
@@ -25,9 +20,6 @@ describe('protectionService', () => {
   let storageService: SNStorageService
   let itemManager: ItemManager
   let internalEventBus: InternalEventBusInterface
-  let payloadManager: PayloadManager
-  let deviceInterface: DeviceInterface
-  let fakeCrypto: SNPureCrypto
   let protectionService: SNProtectionService
 
   const createService = () => {
@@ -56,44 +48,73 @@ describe('protectionService', () => {
   beforeEach(() => {
     setupRandomUuid()
 
-    internalEventBus = new InternalEventBus()
+    internalEventBus = {} as jest.Mocked<InternalEventBus>
+    itemManager = {} as jest.Mocked<ItemManager>
 
-    payloadManager = new PayloadManager(internalEventBus)
-
-    itemManager = new ItemManager(payloadManager, internalEventBus)
-
-    deviceInterface = {} as jest.Mocked<DeviceInterface>
+    challengeService = {} as jest.Mocked<ChallengeService>
+    challengeService.promptForChallengeResponse = jest.fn()
 
     storageService = {} as jest.Mocked<SNStorageService>
     storageService.getValue = jest.fn()
 
-    fakeCrypto = {} as jest.Mocked<SNPureCrypto>
-
-    protocolService = new SNProtocolService(
-      itemManager,
-      payloadManager,
-      deviceInterface,
-      storageService,
-      'test',
-      fakeCrypto,
-      internalEventBus,
-    )
+    protocolService = {} as jest.Mocked<SNProtocolService>
+    protocolService.hasAccount = jest.fn().mockReturnValue(true)
+    protocolService.hasPasscode = jest.fn().mockReturnValue(false)
   })
 
   describe('files', () => {
-    it('only protected file should require auth', () => {
+    it('unprotected file should require auth', async () => {
+      protectionService = createService()
+
+      const unprotectedFile = createFile('protected.txt', false)
+
+      await protectionService.authorizeProtectedActionForFiles(
+        [unprotectedFile],
+        ChallengeReason.AccessProtectedFile,
+      )
+
+      expect(challengeService.promptForChallengeResponse).not.toHaveBeenCalled()
+    })
+
+    it('protected file should require auth', async () => {
+      protectionService = createService()
+
+      const protectedFile = createFile('protected.txt', true)
+
+      await protectionService.authorizeProtectedActionForFiles(
+        [protectedFile],
+        ChallengeReason.AccessProtectedFile,
+      )
+
+      expect(challengeService.promptForChallengeResponse).toHaveBeenCalled()
+    })
+
+    it('array of files having one protected should require auth', async () => {
       protectionService = createService()
 
       const protectedFile = createFile('protected.txt', true)
       const unprotectedFile = createFile('unprotected.txt', false)
 
-      const protectedFileNeedsAuthorization =
-        protectedFile.protected && !protectionService.hasUnprotectedAccessSession()
-      const unprotectedFileNeedsAuthorization =
-        unprotectedFile.protected && !protectionService.hasUnprotectedAccessSession()
+      await protectionService.authorizeProtectedActionForFiles(
+        [protectedFile, unprotectedFile],
+        ChallengeReason.AccessProtectedFile,
+      )
 
-      expect(protectedFileNeedsAuthorization).toBe(true)
-      expect(unprotectedFileNeedsAuthorization).toBe(false)
+      expect(challengeService.promptForChallengeResponse).toHaveBeenCalled()
+    })
+
+    it('array of files having none protected should require auth', async () => {
+      protectionService = createService()
+
+      const protectedFile = createFile('protected.txt', false)
+      const unprotectedFile = createFile('unprotected.txt', false)
+
+      await protectionService.authorizeProtectedActionForFiles(
+        [protectedFile, unprotectedFile],
+        ChallengeReason.AccessProtectedFile,
+      )
+
+      expect(challengeService.promptForChallengeResponse).not.toHaveBeenCalled()
     })
   })
 })
