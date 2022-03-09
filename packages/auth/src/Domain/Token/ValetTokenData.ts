13a22d9734f898d0cd2475fbe53a021be2c20a5f
Karol Sójko <karolsojko@protonmail.com>
2022-03-09 16:42:56 +0100

    fix: add deleting as a valid operation in valet token

--- packages/auth/src/Domain/Token/ValetTokenData.ts
+++ packages/auth/src/Domain/Token/ValetTokenData.ts
@@ -2,7 +2,7 @@ import { Uuid } from '@standardnotes/common'
 
 export type ValetTokenData = {
   userUuid: Uuid,
-  permittedOperation: 'read' | 'write',
+  permittedOperation: 'read' | 'write' | 'delete',
   permittedResources: Array<string>,
   uploadBytesUsed: number,
   uploadBytesLimit: number,
