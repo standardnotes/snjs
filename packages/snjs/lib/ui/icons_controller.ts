4d1aa6ff156c406e1916747e1c17aa1b301fd870
Aman Harwara <amanharwara@protonmail.com>
2022-03-14 00:26:31 +0530

    feat: add utility to get icon type for file mime type

--- packages/snjs/lib/ui/icons_controller.ts
+++ packages/snjs/lib/ui/icons_controller.ts
@@ -8,6 +8,44 @@ export class IconsController {
   // eslint-disable-next-line @typescript-eslint/no-empty-function
   public deinit(): void {}
 
+  getIconForFileType(type: string): IconType {
+    let iconType: IconType = 'file-other'
+
+    if (type === 'application/pdf') {
+      iconType = 'file-pdf'
+    }
+
+    if (/word/.test(type)) {
+      iconType = 'file-doc'
+    }
+
+    if (/powerpoint|presentation/.test(type)) {
+      iconType = 'file-ppt'
+    }
+
+    if (/excel|spreadsheet/.test(type)) {
+      iconType = 'file-xls'
+    }
+
+    if (/^image\//.test(type)) {
+      iconType = 'file-image'
+    }
+
+    if (/^video\//.test(type)) {
+      iconType = 'file-mov'
+    }
+
+    if (/^audio\//.test(type)) {
+      iconType = 'file-music'
+    }
+
+    if (/(zip)|([tr]ar)|(7z)/.test(type)) {
+      iconType = 'file-zip'
+    }
+
+    return iconType
+  }
+
   getIconAndTintForEditor(identifier: FeatureIdentifier | undefined): [IconType, number] {
     switch (identifier) {
       case FeatureIdentifier.BoldEditor:
