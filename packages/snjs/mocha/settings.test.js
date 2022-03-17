0f208b43befc6b7f7950b9d6437a87bd3a5b9af5
Mo <mo@standardnotes.com>
2022-03-17 11:47:31 -0500

    fix: settings tests

--- packages/snjs/mocha/settings.test.js
+++ packages/snjs/mocha/settings.test.js
@@ -31,30 +31,30 @@ describe('settings service', function () {
 
   it('throws error on an invalid setting update', async function () {
     const invalidSetting = 'FAKE_SETTING'
-    let caughtError = null
+    let caughtError = undefined
     try {
       await snApp.settings.updateSetting(invalidSetting, fakePayload)
     } catch (error) {
       caughtError = error
     }
 
-    expect(caughtError).not.to.equal(null)
+    expect(caughtError).not.to.equal(undefined)
   })
 
   it('creates and lists settings', async function () {
     await snApp.settings.updateSetting(validSetting, fakePayload)
     const responseList = await snApp.settings.listSettings()
-    expect(responseList).to.eql({ [validSetting]: fakePayload })
+    expect(responseList.getSettingValue(validSetting)).to.eql(fakePayload)
   })
 
   it('creates and deletes a setting', async function () {
     await snApp.settings.updateSetting(validSetting, fakePayload)
-    const responseCreate = await snApp.getSetting(validSetting)
+    const responseCreate = await snApp.settings.getSetting(validSetting)
     expect(responseCreate).to.eql(fakePayload)
 
     await snApp.settings.deleteSetting(validSetting)
     const responseDeleted = await snApp.settings.listSettings()
-    expect(responseDeleted).to.eql({})
+    expect(responseDeleted.getSettingValue(validSetting)).to.not.be.ok
   })
 
   it('creates and updates a setting', async function () {
@@ -66,7 +66,7 @@ describe('settings service', function () {
 
   it('reads a nonexistent setting', async () => {
     const setting = await snApp.settings.getSetting(validSetting)
-    expect(setting).to.equal(null)
+    expect(setting).to.equal(undefined)
   })
 
   it('reads a nonexistent sensitive setting', async () => {
@@ -87,6 +87,9 @@ describe('settings service', function () {
       MuteFailedBackupsEmailsOption.Muted,
     )
     const settings = await snApp.settings.listSettings()
-    expect(settings).to.eql({ MUTE_FAILED_BACKUPS_EMAILS: 'muted' })
+    expect(settings.getSettingValue(SettingName.MuteFailedBackupsEmails)).to.eql(
+      MuteFailedBackupsEmailsOption.Muted,
+    )
+    expect(settings.getSettingValue(SettingName.MfaSecret)).to.not.be.ok
   })
 })
