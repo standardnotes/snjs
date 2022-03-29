d7883ec574737ed28e47080880eef3f55be4d92f
Karol Sójko <karolsojko@protonmail.com>
2022-03-29 09:35:38 +0200

    fix: remove subscription name and expiration date - to be retrieved upon acceptance

--- packages/domain-events/src/Domain/Event/SharedSubscriptionInvitationCreatedEventPayload.ts
+++ packages/domain-events/src/Domain/Event/SharedSubscriptionInvitationCreatedEventPayload.ts
@@ -1,10 +1,6 @@
-import { SubscriptionName } from '@standardnotes/common'
-
 export interface SharedSubscriptionInvitationCreatedEventPayload {
   inviterEmail: string
   inviterSubscriptionId: number
-  inviterSubscriptionName: SubscriptionName
-  inviterSubscriptionExpiresAt: number
   inviteeIdentifier: string
   inviteeIdentifierType: 'email' | 'hash'
   sharedSubscriptionInvitationUuid: string
