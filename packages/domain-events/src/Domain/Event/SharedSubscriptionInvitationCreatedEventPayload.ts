935268ab0541d314c0c816db92d20ce647ed85bc
Karol Sójko <karolsojko@protonmail.com>
2022-03-29 09:19:33 +0200

    fix: add subscription name and expiration date to the shared subscription invitation created event

--- packages/domain-events/src/Domain/Event/SharedSubscriptionInvitationCreatedEventPayload.ts
+++ packages/domain-events/src/Domain/Event/SharedSubscriptionInvitationCreatedEventPayload.ts
@@ -1,6 +1,10 @@
+import { SubscriptionName } from '@standardnotes/common'
+
 export interface SharedSubscriptionInvitationCreatedEventPayload {
   inviterEmail: string
   inviterSubscriptionId: number
+  inviterSubscriptionName: SubscriptionName
+  inviterSubscriptionExpiresAt: number
   inviteeIdentifier: string
   inviteeIdentifierType: 'email' | 'hash'
   sharedSubscriptionInvitationUuid: string
