b12f257b02d46ad9c717e6c51d6e7ca7e9c06959
Mo <mo@standardnotes.com>
2022-04-13 12:05:46 -0500

    fix: emit changed deleted items as removed (#691)
    * fix: emit changed deleted items as removed
    
    * fix: create item call

--- packages/models/src/Domain/Runtime/Index/ItemDelta.ts
+++ packages/models/src/Domain/Runtime/Index/ItemDelta.ts
@@ -8,6 +8,10 @@ import {
 export interface ItemDelta<C extends ItemContent = ItemContent> {
   changed: (DecryptedItemInterface<C> | EncryptedItemInterface | DeletedItemInterface)[]
   inserted: (DecryptedItemInterface<C> | EncryptedItemInterface | DeletedItemInterface)[]
+  /** Items that were deleted and finished sync */
   discarded: DeletedItemInterface[]
+  /** Items which have encrypted overwrite protection enabled */
   ignored: EncryptedItemInterface[]
+  /** Items which were previously error decrypting which have now been successfully decrypted */
+  unerrored: DecryptedItemInterface<C>[]
 }
