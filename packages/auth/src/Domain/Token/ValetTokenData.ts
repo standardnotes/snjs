e05deb9cd3fbc69e90fd2dbcae5a62582febe5fa
Karol Sójko <karolsojko@protonmail.com>
2022-04-15 13:16:52 +0200

    fix: add subscription uuids to valet token and file removed events for cleanup purposes

--- packages/auth/src/Domain/Token/ValetTokenData.ts
+++ packages/auth/src/Domain/Token/ValetTokenData.ts
@@ -2,6 +2,8 @@ import { Uuid } from '@standardnotes/common'
 
 export type ValetTokenData = {
   userUuid: Uuid,
+  performerSubscriptionUuid: Uuid
+  regularSubscriptionUuid: Uuid
   permittedOperation: 'read' | 'write' | 'delete',
   permittedResources: Array<{
     remoteIdentifier: string,
--- packages/domain-events/src/Domain/Event/FileRemovedEventPayload.ts
+++ packages/domain-events/src/Domain/Event/FileRemovedEventPayload.ts
@@ -2,6 +2,7 @@ import { Uuid } from '@standardnotes/common'
 
 export interface FileRemovedEventPayload {
   userUuid: Uuid
+  regularSubscriptionUuid: Uuid
   fileByteSize: number
   filePath: string
   fileName: string
