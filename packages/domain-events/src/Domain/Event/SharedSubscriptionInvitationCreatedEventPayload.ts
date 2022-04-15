387c460330ebf430ddf0a4fa1e27ec6a49f64e5a
Karol Sójko <karolsojko@protonmail.com>
2022-04-15 10:11:09 +0200

    fix: both shared subscription events to include invitee uuid

--- packages/domain-events/src/Domain/Event/SharedSubscriptionInvitationCreatedEventPayload.ts
+++ packages/domain-events/src/Domain/Event/SharedSubscriptionInvitationCreatedEventPayload.ts
@@ -2,6 +2,6 @@ export interface SharedSubscriptionInvitationCreatedEventPayload {
   inviterEmail: string
   inviterSubscriptionId: number
   inviteeIdentifier: string
-  inviteeIdentifierType: 'email' | 'hash'
+  inviteeIdentifierType: 'email' | 'hash' | 'uuid'
   sharedSubscriptionInvitationUuid: string
 }
