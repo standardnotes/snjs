20b03e6346b2b4748ec85eca940f7d207727455d
Mo <mo@standardnotes.com>
2022-04-19 11:15:08 -0500

    refactor: remove version from decrypted payloads (#697)

--- packages/models/src/Domain/Abstract/Item/Implementations/DecryptedItem.ts
+++ packages/models/src/Domain/Abstract/Item/Implementations/DecryptedItem.ts
@@ -40,10 +40,6 @@ export class DecryptedItem<C extends ItemContent = ItemContent>
     this.locked = this.getAppDomainValueWithDefault(AppDataField.Locked, false)
   }
 
-  get version() {
-    return this.payload.version
-  }
-
   public static DefaultAppDomain() {
     return DefaultAppDomain
   }
