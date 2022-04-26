06e276fdd260a56ff54c4d64a94ef2f2406b8414
Mo <mo@standardnotes.com>
2022-04-26 14:46:36 -0500

    test: fix test interfacE

--- packages/snjs/lib/Application/ApplicationGroup.spec.ts
+++ packages/snjs/lib/Application/ApplicationGroup.spec.ts
@@ -1,11 +1,6 @@
 import { SNApplicationGroup } from './ApplicationGroup'
 import { DeviceInterface, InternalEventBusInterface } from '@standardnotes/services'
 
-class MockApplicationGroup extends SNApplicationGroup {
-  // eslint-disable-next-line @typescript-eslint/no-empty-function
-  handleAllWorkspacesSignedOut() {}
-}
-
 describe.skip('application group', () => {
   const createDevice = () => {
     return {} as jest.Mocked<DeviceInterface>
@@ -20,6 +15,6 @@ describe.skip('application group', () => {
     internalEventBus = {} as jest.Mocked<InternalEventBusInterface>
     internalEventBus.publish = jest.fn()
 
-    new MockApplicationGroup(device, internalEventBus)
+    new SNApplicationGroup(device, internalEventBus)
   })
 })
