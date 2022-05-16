900475e669ebb1ae14043c2335e644d5c7f2fbdb
Karol Sójko <karolsojko@protonmail.com>
2022-05-16 12:19:56 +0200

    fix: newline string formatting in test

--- packages/snjs/mocha/auth.test.js
+++ packages/snjs/mocha/auth.test.js
@@ -43,8 +43,9 @@ describe('basic auth', function () {
       error = caughtError
     }
 
-    expect(error.message).to.equal('Your password must be at least 8 characters in length. \
-    For your security, please choose a longer password or, ideally, a passphrase, and try again.')
+    console.log(error.message)
+    expect(error.message).to.equal('Your password must be at least 8 characters in length. '
+    + 'For your security, please choose a longer password or, ideally, a passphrase, and try again.')
 
     expect(await this.application.protocolService.getRootKey()).to.not.be.ok
   })
