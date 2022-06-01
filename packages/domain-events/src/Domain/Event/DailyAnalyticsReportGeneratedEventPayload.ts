b9f3b8dc258770b214f369cea08358bb05a55179
Karol Sójko <karolsojko@protonmail.com>
2022-06-01 11:06:22 +0200

    feat: renamed daily version adoption report to daily analytics report

--- packages/domain-events/src/Domain/Event/DailyAnalyticsReportGeneratedEventPayload.ts
+++ packages/domain-events/src/Domain/Event/DailyAnalyticsReportGeneratedEventPayload.ts
@@ -1,4 +1,4 @@
-export interface DailyVersionAdoptionReportGeneratedEventPayload {
+export interface DailyAnalyticsReportGeneratedEventPayload {
   snjsStatistics: Array<{
     version: string
     count: number
@@ -10,6 +10,7 @@ export interface DailyVersionAdoptionReportGeneratedEventPayload {
   activityStatistics: Array<{
     name: string
     retention: number
+    totalCount: number
   }>
   outOfSyncIncidents: number
 }
--- packages/domain-events/src/Domain/Event/DailyVersionAdoptionReportGeneratedEvent.ts
+++
@@ -1,7 +0,0 @@
-import { DomainEventInterface } from './DomainEventInterface'
-import { DailyVersionAdoptionReportGeneratedEventPayload } from './DailyVersionAdoptionReportGeneratedEventPayload'
-
-export interface DailyVersionAdoptionReportGeneratedEvent extends DomainEventInterface {
-  type: 'DAILY_VERSION_ADOPTION_REPORT_GENERATED'
-  payload: DailyVersionAdoptionReportGeneratedEventPayload
-}
