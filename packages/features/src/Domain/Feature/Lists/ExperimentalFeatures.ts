600c88a81e5871a91793c2e7ca2aea9f358d839e
Johnny A <5891646+johnny243@users.noreply.github.com>
2022-06-09 11:08:55 -0400

    feat: advanced checklist (#759)
    * feat: advanced checklist
    
    * fix: remove invalid entry from checksums.json
    
    Co-authored-by: Johnny Almonte <johnny243@users.noreply.github.com>

--- packages/features/src/Domain/Feature/Lists/ExperimentalFeatures.ts
+++ packages/features/src/Domain/Feature/Lists/ExperimentalFeatures.ts
@@ -1,5 +1,27 @@
-import { FeatureDescription } from '../FeatureDescription'
+import { SubscriptionName } from '@standardnotes/common'
+import { NoteType } from '../../Component/NoteType'
+import { PermissionName } from '../../Permission/PermissionName'
+import { EditorFeatureDescription, FeatureDescription } from '../FeatureDescription'
+import { FeatureIdentifier } from '../FeatureIdentifier'
+import { FillEditorComponentDefaults } from './Utilities/FillEditorComponentDefaults'
 
 export function experimentalFeatures(): FeatureDescription[] {
-  return []
+  const advancedChecklist: EditorFeatureDescription = FillEditorComponentDefaults({
+    availableInSubscriptions: [SubscriptionName.PlusPlan, SubscriptionName.ProPlan],
+    name: 'Advanced Checklist [Alpha]',
+    identifier: FeatureIdentifier.AdvancedChecklist,
+    note_type: NoteType.Task,
+    version: '0.0.1',
+    spellcheckControl: true,
+    file_type: 'json',
+    interchangeable: false,
+    permission_name: PermissionName.AdvancedChecklist,
+    description: 'A task editor with grouping functionality.',
+    git_repo_url: 'https://github.com/standardnotes/advanced-checklist',
+    marketing_url: 'https://github.com/standardnotes/advanced-checklist',
+    static_files: ['build'],
+    index_path: 'build/index.html',
+  })
+
+  return [advancedChecklist]
 }
