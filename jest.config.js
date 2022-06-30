bfa0f8f0c29876fd0428fd8c3537320ef1c269d9
Karol Sójko <karolsojko@gmail.com>
2022-06-30 11:59:16 +0200

    feat: add initial pnp setup

---
+++ jest.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  testRegex: '(/__tests__/.*|(\\.|/)(test|spec))\\.ts$',
+  globals: {
+    'ts-jest': {
+      tsconfig: './linter.tsconfig.json',
+    },
+  },
+  testTimeout: 20000,
+  coverageThreshold: {
+    global: {
+      branches: 100,
+      functions: 100,
+      lines: 100,
+      statements: 100,
+    },
+  },
+};
